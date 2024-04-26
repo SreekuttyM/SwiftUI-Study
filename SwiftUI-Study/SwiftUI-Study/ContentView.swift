b7c8f30d1e92bdfa5850a99574fa4194769fa37c
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-26 22:24:43 +0400

    save NavigationStack paths using Codable

--- SwiftUI-Study/SwiftUI-Study/ContentView.swift
+++ SwiftUI-Study/SwiftUI-Study/ContentView.swift
@@ -8,27 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var path = [Int]()
-    
+    @State private var pathStore = PathStore()
+
     var body: some View {
-        NavigationStack(path: $path) {
-            VStack {
-                // more code to come
-                Button("Show 32") {
-                    path = [32]
+        NavigationStack(path: $pathStore.path) {
+            DetailView(number: 0)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i)
                 }
-                
-                Button("Show 64") {
-                    path.append(64)
-                }
-                
-                Button("Show 32 then 64") {
-                    path = [32, 64]
-                }
-            }
-            .navigationDestination(for: Int.self) { selection in
-                Text("You selected \(selection)")
-            }
         }
     }
 }
